2b9e3d3d301a1e74d3c8c54bdd7c4cd8c3866b11
kirabelmont <38549865+kirabelmont@users.noreply.github.com>
2022-05-23 15:55:30 -0500

    Update videocreator.dart

--- lib/videocreator.dart
+++ lib/videocreator.dart
@@ -2,4 +2,4 @@ library videocreator;
 
 export 'videocreator_archives/screen_recorder.dart';
 export 'videocreator_archives/FfmpegProvider.dart';
-export 'videocreator_archives/render_type.dart';
+export 'package:videocreator/videocreator_archives/render_type.dart';
